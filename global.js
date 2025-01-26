1c2283c2f73b8fcbddbcad96a227e03a465c9d34
achinnam09 <abhinav.chinnam09@gmail.com>
2025-01-25 18:20:58 -0800

    auto nav bar changes

--- global.js
+++ global.js
@@ -4,3 +4,47 @@ function $$(selector, context=document){
     return Array.from(context.querySelectorAll(selector))
 }
 
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// if (currentLink){
+//     currentLink.classList.add("current");
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/achinnam09', title: 'Github' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !document.documentElement.classList.contains('home') && !url.startsWith('http')
+        ? '../' + url
+        : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+    
+    nav.append(a);
+}
+
