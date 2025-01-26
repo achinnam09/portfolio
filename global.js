45877fabe8144a412ddf4ffccf14ceafebd3e512
achinnam09 <abhinav.chinnam09@gmail.com>
2025-01-25 19:09:27 -0800

    dark mode implementation

--- global.js
+++ global.js
@@ -48,3 +48,35 @@ for (let p of pages) {
     nav.append(a);
 }
 
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+        Theme:
+        <select id="theme-switcher">
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>
+    `
+);
+
+const select = document.querySelector('#theme-switcher')
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', colorScheme);
+    localStorage.colorScheme = colorScheme;
+}
+
+if('colorScheme' in localStorage){
+    const savedScheme = localStorage.colorScheme;
+    setColorScheme(savedScheme);
+    select.value = savedScheme;
+}
+
+select.addEventListener('input', function (event) {
+    setColorScheme(event.target.value);
+});
+
+
