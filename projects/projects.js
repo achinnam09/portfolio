babd3957f919f2f74ac6a032871c76debe33afbe
achinnam09 <abhinav.chinnam09@gmail.com>
2025-01-31 18:12:33 -0800

    dynamically changes project page title to match count of projects

--- projects/projects.js
+++ projects/projects.js
@@ -4,4 +4,12 @@ const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
+
+if (projects && projects.length > 0) {
+    projectsTitle.textContent = '${projects.length} Projects';
+
+} else {
+    projectsTitle.textContent = 'No Projects Found';
+}
+
 renderProjects(projects, projectsContainer, 'h2');
