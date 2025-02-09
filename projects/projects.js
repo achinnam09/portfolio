5c4360ed5996cd7461e7117bf33176ceb087047e
achinnam09 <abhinav.chinnam09@gmail.com>
2025-02-08 16:57:26 -0800

    static pie chart on projects index.html using d3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,7 @@
 import {fetchJSON, renderProjects} from '../global.js';
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
 (async function loadProjects() {
     try {
         const projects = await fetchJSON('../lib/projects.json');
@@ -17,3 +20,31 @@ import {fetchJSON, renderProjects} from '../global.js';
         console.error('Error fetching projects:', error);
     }
 })();
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10); 
+// let total = 0;
+// for (let d of data) {
+//     total += d;
+// }
+// let angle = 0;
+// let arcData = [];
+// for (let d of data) {
+//     let endAngle = angle + (d / total) * 2 * Math.PI;
+//     arcData.push({startAngle: angle, endAngle});
+//     angle = endAngle;
+// }
+// let arcs = arcData.map((d) => arcGenerator(d));
+// let colors = ['gold', 'blue'];
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+})
