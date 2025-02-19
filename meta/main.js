d446b103da7c6e208ddd0ba49f5a4667d9caaba6
achinnam09 <abhinav.chinnam09@gmail.com>
2025-02-19 14:01:38 -0800

    computing commit stats

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
     data = await d3.csv('loc.csv', (row) => ({
@@ -9,8 +10,44 @@ async function loadData() {
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
+
+    commits = processCommits();
+    console.log(commits);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadData();
 });
+
+
+function processCommits() {
+    commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+        let first = lines[0];
+
+        let {author, date, time, timezone, datetime} = first;
+
+        let ret = {
+            id: commit,
+            url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+            author,
+            date,
+            time,
+            timezone,
+            datetime,
+            hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+            totalLines: lines.length,
+        };
+
+        Object.defineProperty(ret, 'lines', {
+            value: lines,
+            enumerable: false,
+        });
+
+        return ret;
+    });
+
+    return commits;
+}
+
