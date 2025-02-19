84c5beb7b48468c7814b8f14c8227a75f6974c09
achinnam09 <abhinav.chinnam09@gmail.com>
2025-02-19 15:46:43 -0800

    scatterplot for commit activity + styling

--- meta/main.js
+++ meta/main.js
@@ -11,8 +11,10 @@ async function loadData() {
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
-
+    
+    processCommits();
     displayStats();
+    createScatterPlot();
     
 }
 
@@ -53,7 +55,6 @@ function processCommits() {
 }
 
 function displayStats() {
-    processCommits();
 
     const statsContainer = d3.select('#stats')
         .append('div')
@@ -84,4 +85,70 @@ function displayStats() {
         statsGrid.append('dt').text(stat.label);
         statsGrid.append('dd').text(stat.value);
     });
+}
+
+function createScatterPlot() {
+    const width = 1000;
+    const height = 600;
+    
+    const margin = { top: 10, right: 10, bottom: 30, left: 50 };
+
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom
+    };
+
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([usableArea.left, usableArea.right])
+        .nice();
+    
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, 24])
+        .range([usableArea.bottom, usableArea.top]);
+
+    const gridlines = svg
+        .append('g')
+        .attr('class', 'gridlines')
+        .attr('transform', `translate(${usableArea.left}, 0)`);  // Fix translation
+
+    gridlines.call(
+        d3.axisLeft(yScale)
+            .tickFormat('')
+            .tickSize(-usableArea.width)
+    );
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${usableArea.bottom})`)
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('transform', `translate(${usableArea.left}, 0)`)
+        .call(yAxis);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots.selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
 }
